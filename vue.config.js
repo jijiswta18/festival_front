b1c1f197eec568d3270a4640b7a0cb69be2cd2e9
Jeedev <jijiswta18@gmail.com>
2022-09-16 14:04:08 +0700

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,6 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: [
-//     'vuetify'
-//   ]
-// })
-
-module.exports = {
-  productionSourceMap: false,  
-  outputDir: '../dist',
-  assetsDir: 'static',
-  indexPath: 'public/index.html'
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: [
+    'vuetify'
+  ]
+})
